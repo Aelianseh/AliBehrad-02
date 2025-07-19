50264bc932e64563248dc078da98d5c8d1dfd7ea
ALi Behrad <aeliansed@gmail.com>
2025-07-19 23:58:05 +0330

    responsive and some changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,32 +1,54 @@
-// import "./Header.css";
 import { Link } from "react-router-dom";
 import style from "./Header.module.css";
-function Header(probs) {
-    // console.log(probs);
+import { useState } from "react";
+import { FaTimesCircle } from "react-icons/fa";
+import { TiThMenuOutline } from "react-icons/ti";
+
+function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+    // console.log(menuOpen);
+
     return (
-        <div className={style.header}>
-            <div className="container">
-                <ul>
-                    <li>
-                        <Link to="/">HOME
-                        </Link>
-                    </li>
 
+        <div>
+            <div className={style.header}>
+                <div className="container">
+                    <div className={style.menuIcon} onClick={toggleMenu}>
+                        {menuOpen ? <FaTimesCircle /> :
+                            <TiThMenuOutline />
+                        }
 
-                    <li>
-                        <Link to="/aboutme">AboutMe
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/linkedin">Linkedin</Link>
-                    </li>
-                    <li>
-                        <Link to="/email">E-mail
-                        </Link>
-                    </li>
-                </ul>
+                        <div>
+                            {/* {`${console.log(menuOpen)}`} */}
+                        </div>
+
+                    </div>
+                    <div className={style.navmenuwrap}>
+                        <ul className={`${style.navmenu}  ${menuOpen ? style.humbopen : ""}`}>
+                            <li>
+                                <Link to="/">HOME
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/aboutme">AboutMe
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/linkedin">Linkedin</Link>
+                            </li>
+                            <li>
+                                <Link to="/email">E-mail
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
+
     )
 }
 export default Header;
