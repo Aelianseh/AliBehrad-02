4f3376dff06e3e3389288f66e52b20fd00b4221a
ALi Behrad <aeliansed@gmail.com>
2025-08-01 23:48:16 +0330

    darkmode-first

---
+++ src/Components/Darklight/Darklight.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import style from "./Darklight.module.css"
+import { FaMoon, FaSun } from "react-icons/fa"
+function Darklight() {
+    const [islight, setIslight] = useState(false)
+    const lightToggle = () => {
+        setIslight(!islight)
+        console.log(islight);
+    };
+
+    useEffect(() => {
+        if (islight) {
+            document.body.classList.add("dark");
+            console.log(islight);
+
+        } else {
+            document.body.classList.remove("dark");
+        }
+    }, [islight])
+    return (
+        <div className={style.wrapper}>
+            <button onClick={lightToggle} className={`${islight ? style.active : style.noactive}`}>
+                {!islight ? <FaSun /> : <FaMoon />}</button>
+        </div>
+    )
+}
+export default Darklight
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,12 +5,14 @@ import Expn from "../../Components/Expn/Expn"
 import Resume from "../../Components/Resume/Resume"
 import Navbar from "../../Components/Navbar/Navbar"
 import Intr from "../../Components/Intr/Intr"
+import Darklight from "../../Components/Darklight/Darklight"
 function Home() {
-    
+
     return (
         <section>
             <div>
                 <Navbar />
+                <Darklight />
                 <Header />
                 <Intro />
                 <Intr />
